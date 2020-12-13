3d202152e0c69194a10fa1420aa17475ccd90423
olayile <OLAYILEEJEKWU@GMAIL.COM>
2020-12-13 08:24:02 +0200

    NB and RF models working

--- main.py
+++ main.py
@@ -8,6 +8,7 @@ from wordcloud import WordCloud, STOPWORDS, ImageColorGenerator
 from sklearn.feature_extraction.text import TfidfVectorizer
 
 
+
 # title_vectorizer = open("final_news_cv_vectorizer.pkl","rb")
 # title_cv = joblib.load(title_vectorizer)
 
@@ -52,6 +53,28 @@ def main():
             # vect_text = vect.transform(paper_title)
             prediction = predictor.predict(vect_text)
             st.write(prediction)
+            st.success("Your predicted Journal is: {}".format(prediction[0]))
+
+        elif model_choice == 'NB':
+            predictor = load_prediction_models("NB_model.sav")
+            vect = load_prediction_models('vect_model.sav')
+            tf1_new = TfidfVectorizer(max_features = None, vocabulary = vect.vocabulary_)
+            vect_text = tf1_new.fit_transform(paper_title)
+
+            # vect_text = vect.transform(paper_title)
+            prediction = predictor.predict(vect_text)
+            st.write(prediction)
+            st.success("Your predicted Journal is: {}".format(prediction[0]))
+
+        elif model_choice == 'RF':
+            predictor = load_prediction_models("rf_model.sav")
+            vect = load_prediction_models('vect_model.sav')
+            tf1_new = TfidfVectorizer(max_features = None, vocabulary = vect.vocabulary_)
+            vect_text = tf1_new.fit_transform(paper_title)
+
+            # vect_text = vect.transform(paper_title)
+            prediction = predictor.predict(vect_text)
+            st.write(prediction)
 
             # final_result = get_key(prediction,prediction_labels)
             st.success("Your predicted Journal is: {}".format(prediction[0]))
@@ -59,6 +82,14 @@ def main():
 
     st.sidebar.title("Journal Paper Classifier")
     st.sidebar.markdown('How does it work?')
+    st.sidebar.markdown('Finding and selecting a suitable academic journal is always challenging especially for young researchers')
+    st.sidebar.markdown('Various classification algorithms will be used to research papers by journal along with feature selection and dimensionality reduction methods using scikit-learn')
+    # annotated_text(
+    # "The development of a ",
+    # ("Bioreactor", "aligned word", "#8ef"),
+    # " for ",
+    # ("biocatalyst", "aligned word", "#faa"),
+    # ("production"))
 
 
 
